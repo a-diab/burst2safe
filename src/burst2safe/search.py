0a46f876d17738135f4a2bd7cf2eef61b92fdfcc
Joseph H Kennedy <me@jhkennedy.org>
2026-02-11 08:02:57 -0900

    Apply suggestion from @jhkennedy

--- src/burst2safe/search.py
+++ src/burst2safe/search.py
@@ -8,7 +8,7 @@ import numpy as np
 from asf_search.Products.S1BurstProduct import S1BurstProduct
 from shapely.geometry import Polygon
 
-# Disable noise `asf_search` logger
+# Disable noisy `asf_search` logger
 asf_logger = logging.getLogger(asf_search.__name__)
 asf_logger.disabled = True
 
